f30d97da7aa6b03c7fe40ab49ebd06020da6781a
umeyr13 <umeyr.gulcimen@gmail.com>
2023-05-02 14:05:06 +0300

    Kayıt ve Aktivasyon Mail i, giriş - çıkış

--- MakaleBLL/KullanıcıYonet.cs
+++ MakaleBLL/KullanıcıYonet.cs
@@ -1,6 +1,8 @@
 ﻿using Makale_Entities;
 using Makale_Entities.ViewModel;
+using MakaleCommon;
 using MakaleDataAccessLayer;
+using Microsoft.Win32.SafeHandles;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,31 @@ namespace MakaleBLL
     public class KullanıcıYonet
     {
         Repository<Kullanici> rep_kul = new Repository<Kullanici>();
-        public MakaleBLLSonuc<Kullanici> KullanıcıBul(RegisterModel model)
+
+        public MakaleBLLSonuc<Kullanici> ActivateUser(Guid id)
+        {
+            MakaleBLLSonuc<Kullanici> sonuc = new MakaleBLLSonuc<Kullanici>();
+           sonuc.nesne =  rep_kul.Find(x => x.AktifGuid == id);
+            if (sonuc.nesne !=null)
+            {
+                if (sonuc.nesne.aktif)
+                {
+                    sonuc.hatalar.Add("Kullanıcı daha önce aktifleştirilmiştir");
+                }
+                else
+                {
+                    sonuc.nesne.aktif=true;
+                    rep_kul.Update(sonuc.nesne);
+                }
+            }
+            else
+            {
+                sonuc.hatalar.Add("Aktifleştirilecek kullanıcı bulunamadı");
+            }
+            return sonuc;
+        }
+
+        public MakaleBLLSonuc<Kullanici> KullanıcıKaydet(RegisterModel model)
         {
             MakaleBLLSonuc<Kullanici> sonuc = new MakaleBLLSonuc<Kullanici>();
             Kullanici k =rep_kul.Find(x => x.KullaniciAdi == model.KullaniciAdi || x.Email==model.email);
@@ -30,8 +56,61 @@ namespace MakaleBLL
 
                 }
             }
+            else // eğer database de aynı kullanıcı yoksa buraya gelir.
+            {
+               int islemsonuc = rep_kul.Insert(new Kullanici()
+                {
+                    KullaniciAdi = model.KullaniciAdi
+                   ,Email = model.email
+                   ,Sifre = model.Sifre
+                   ,AktifGuid = Guid.NewGuid()
+                   //Bunları Repository/Insert metot u na taşıyabiliriz...
+                   //,KayitTarihi = DateTime.Now
+                   //,DegistirmeTarihi = DateTime.Now
+                   //,DegistirenKullanici ="system" 
+                });
+                //burada iken aslında sonuc nesnesine birşey atılmamış oldu.
 
+                if (islemsonuc >0) //Insert de hata yoksa mail göner
+                {
+                    sonuc.nesne = rep_kul.Find(x =>x.KullaniciAdi == model.KullaniciAdi || x.Email==model.email );
+                    //Eğer İşlem gerçekleşirse sonuç nesnesi boşkalmasın diye kullanıcıyı içine attık.
+
+                    //Aktivasyon mail i atabiliriz.
+                    
+                   string siteURL = ConfigHelper.Get<string>("SiteRootUri");
+
+                    string aktiveUrl = $"{siteURL}/Home/HesapAktiflestir/{sonuc.nesne.AktifGuid}";
+                    string body = $"Merhaba {sonuc.nesne.Ad} {sonuc.nesne.Soyad} <br /> Hesabınızı aktifleştirmek için " +
+                        $"<a href='{aktiveUrl}' target='_blank' > Tıklayınız.. </a>";
+
+                    MailHelper.SendMail(body,sonuc.nesne.Email,"Hesap Aktifleştirme");
+                }
+            
+            }
+
+            return sonuc;//else den gelirse boş nesne gelirdi.
+        }
+
+        public MakaleBLLSonuc<Kullanici> LoginKontrol(LoginModel model)
+        {
+            MakaleBLLSonuc<Kullanici> sonuc = new MakaleBLLSonuc<Kullanici>();
+            sonuc.nesne = rep_kul.Find(x => x.KullaniciAdi == model.KullaniciAdi && x.Sifre==model.Sifre);
+
+            if (sonuc.nesne ==null)
+            {
+                sonuc.hatalar.Add("Hatalı kullanıcı adı veya şifre");
+                
+            }
+            else
+            {
+                if (!sonuc.nesne.aktif)
+                {
+                    sonuc.hatalar.Add("Aktifleştirilmedi ");
+                }
+            }
             return sonuc;
+
         }
 
 
